c1fc233abdb7813d45175383e86320332e835e64
Michael Chesterton <michael.chesterton@gmail.com>
2018-08-18 02:18:49 +1000

    1098200003 doesn't exist, use 1098200003

--- vroom-express.sh
+++ vroom-express.sh
@@ -3,7 +3,7 @@
 # https://github.com/docker/docker/issues/6880
 cat <> /vroom-express/logpipe 1>&2 &
 
-if [ "$UID" -eq 0 ]; then
+if [ $(id -u) -eq 0 ]; then
     exec su-exec osm node /vroom-express/src/index.js
 else
     exec node /vroom-express/src/index.js
