3e1c9f118c42ddd3b4ae0d21f4168a18d9d95130
Michael Chesterton <michael.chesterton@gmail.com>
2018-08-18 02:03:13 +1000

    test for root before using su-exec

--- vroom-express.sh
+++ vroom-express.sh
@@ -3,4 +3,8 @@
 # https://github.com/docker/docker/issues/6880
 cat <> /vroom-express/logpipe 1>&2 &
 
-exec su-exec osm node /vroom-express/src/index.js
+if [ "$UID" -eq 0 ]; then
+    exec su-exec osm node /vroom-express/src/index.js
+else
+    exec node /vroom-express/src/index.js
+fi
