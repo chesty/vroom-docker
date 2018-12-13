71d62a6a9080f9b49e5e46b65dcb01a44897bc92
Michael Chesterton <michael.chesterton@gmail.com>
2018-12-14 01:20:06 +1100

    switch to gosu

--- vroom-express.sh
+++ vroom-express.sh
@@ -3,8 +3,9 @@
 # https://github.com/docker/docker/issues/6880
 cat <> /vroom-express/logpipe 1>&2 &
 
+
 if [ $(id -u) -eq 0 ]; then
-    exec su-exec osm node /vroom-express/src/index.js
+    exec gosu vroom node /vroom-express/src/index.js
 else
     exec node /vroom-express/src/index.js
 fi
